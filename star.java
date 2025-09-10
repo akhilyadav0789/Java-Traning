ffd4645ae59a6e1c99cdbf18dec99b34887491bf
lostkart <akhilyadav0789@gmail.com>
2025-09-10 22:36:33 +0530

    first commit

---
+++ star.java
@@ -0,0 +1,9 @@
+public class star {
+    public static void main(String[] args) {
+        int n = 8; 
+        for (int i = 0; i < n; i++) {          // row loop
+           
+            System.out.print("*"); 
+        }
+    }
+}
